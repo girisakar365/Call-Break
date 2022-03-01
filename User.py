48bfcb36df1b0899b1a1d49f11f0783f0ed8bd3a
Sakar <sakargiri365@gmail.com>
2022-03-01 15:31:55 +0545

    executable

--- User.py
+++ User.py
@@ -1,20 +1,19 @@
 from os import system
 from Cards import all_cards, value
 from random import choice, sample, randint
+from table import show_table
+
+total = 0#total points of bot in a round
 
 class User:
-
-    total = [] #list of total points of bot in a round
-    b_bid = [] #list of bids of bot in a round
-    b = [] #cards of bot
+    b_bid = 0#bids of bot in a round
+    card = None #cards of bot
     
     def __init__(self):
         
         self.all_cards = [i for i in all_cards]
         
         self.points = [] #point tracer
-        
-
         
         self.card_distribution()
 
@@ -32,14 +31,14 @@ class User:
         self.p1 = [i for i in sample(self.all_cards,k = 13)]
         for i in self.p1: self.all_cards.pop(self.all_cards.index(i))
         
-        User.b.append(tuple(self.p1)) # to keep the track of selected cards of bot so as to display to for later!
+        User.card = ' '.join(self.p1) # to keep the track of selected cards of bot so as to display to for later!
 
     def bid(self, bid: int):
         # Bid of user and bot is stored here
         # For now the bid for bot is selected randomly between 1 - 3. 
         self._bid = bid
         self.bid_of_bot = float(randint(1,3))
-        User.b_bid.append(self.bid_of_bot)
+        User.b_bid = self.bid_of_bot
 
     def card_throw(self):
         '''
@@ -49,9 +48,7 @@ class User:
         self.bot = choice(self.p1)
         self.p1.pop(self.p1.index(self.bot))
 
-        return f'''
-bot: {self.bot}
-        '''
+        return f'bot: {self.bot}\n'
 
     def display_user_cards(self):
         '''
@@ -59,7 +56,6 @@ bot: {self.bot}
         If no such colored card then show all sphade cards
         If no sphade card then show all other left over cards 
         '''
-
         try:
             self.to_be_displayed = [i for i in self.user.values() if i[1] == self.bot[1]]
             
@@ -74,9 +70,10 @@ bot: {self.bot}
 
         except AttributeError:
             "Displayes the total cards given to user in the begnning!"
+            print('Your cards: ')
             for key,value in zip(self.user.keys(),self.user.values()):
                 print(f'{key}: {value}',end='   ')
-        print()
+        print('\n')
     
     def take_input(self):
         while True:
@@ -120,24 +117,25 @@ bot: {self.bot}
             if bid of bot == total number of b in self.points + 1: append (bid of bot) + 0.1 in total
         '''
         if self.bid_of_bot == self.points.count('b'):
-            User.total.append(self.bid_of_bot) 
+            total += self.bid_of_bot 
         
         elif self.bid_of_bot < self.points.count('b'):
-            User.total.append(self.bid_of_bot + (self.points.count('b')/10))
+            total += self.bid_of_bot + (self.points.count('b')/10)
         
         elif self.bid_of_bot > self.points.count('b'):
-            User.total.append(-self.bid_of_bot)
+            total -= self.bid_of_bot
 
-for i in range(3):
+for i in range(int(input('Select rounds: '))):
     print()
     user = User()
     user.display_user_cards()
-    user.bid(int(input('Enter your bid: ')))
+    user.bid(int(input('Call a bid: ')))
     system('CLS')
-
     for j in range(13):
         print(user.card_throw())
         user.display_user_cards()
         user.take_input()
-        system('CLS')
+        print('----------------------------------------\n')
     user.result()
+    show_table(user.bid_of_bot, user.total, user.card)
+    print(f'\n-------------------------------- end of round: {i+1} -------------------------------------------\n')
